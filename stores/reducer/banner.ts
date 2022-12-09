6695dffa64aaf5b50403d113f48abde844fcf121
in3166 <in3166@naver.com>
2022-12-09 12:04:49 +0900

    Refactoring: warning 삭제

--- stores/reducer/banner.ts
+++ stores/reducer/banner.ts
@@ -4,32 +4,7 @@ import type { RootState } from '..'
 
 import { IProductItem } from 'types/product'
 
-const INIT_PRODUCT: IProductItem[] = [
-  {
-    id: 'B7N0IjiIJYo',
-    title: 'white and black lego toy',
-    url: 'https://images.unsplash.com/photo-1585366119957-e9730b6d0f60?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzMzI4NzR8MHwxfHNlYXJjaHwxMXx8dG95fGVufDB8fHx8MTY1NDM0MzUwOQ&ixlib=rb-1.2.1&q=80&w=400',
-    date: '2020-03-27T23:28:57-04:00',
-    owner: 'Daniel K Cheung',
-    price: 578,
-  },
-  {
-    id: 'YpNf6ATniQA',
-    title: 'blue red and yellow lego blocks',
-    url: 'https://images.unsplash.com/photo-1603558431750-dfa36513aee6?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzMzI4NzR8MHwxfHNlYXJjaHwxM3x8dG95fGVufDB8fHx8MTY1NDM0MzUwOQ&ixlib=rb-1.2.1&q=80&w=400',
-    date: '2020-10-24T12:54:41-04:00',
-    owner: 'Nick Nice',
-    price: 58,
-  },
-  {
-    id: 'zoyBqT7ytLU',
-    title: 'assorted-colored toys on table',
-    url: 'https://images.unsplash.com/photo-1558060370-d644479cb6f7?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzMzI4NzR8MHwxfHNlYXJjaHwxNHx8dG95fGVufDB8fHx8MTY1NDM0MzUwOQ&ixlib=rb-1.2.1&q=80&w=400',
-    date: '2019-05-16T22:33:06-04:00',
-    owner: 'Huy Hung Trinh',
-    price: 217,
-  },
-]
+const INIT_PRODUCT: IProductItem[] = []
 
 export interface ProductState {
   bannerList: IProductItem[]
