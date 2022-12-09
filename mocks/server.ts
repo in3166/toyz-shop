7c92747fe7929dc1e1836c4a95a90faf59d54ed2
in3166 <in3166@naver.com>
2022-12-09 12:02:16 +0900

    Refactoring: warning 삭제

--- mocks/server.ts
+++ mocks/server.ts
@@ -1,5 +1,5 @@
 import { setupServer } from 'msw/node'
-import { handlers } from './handlers'
+// import { handlers } from './handlers'
 
 // mocking server 생성
-export const server = setupServer(...handlers)
+export const server = setupServer()
