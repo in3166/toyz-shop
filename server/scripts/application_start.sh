e22bbb5c1dd8de8e9853d2a06d3b9ea019af7cfb
in3166 <in3166@naver.com>
2023-03-09 19:53:14 +0900

    Delete: socket server

--- server/scripts/application_start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd /home/ubuntu/github_action
-npm run build
-pm2 start npm --name "toyz_shop" -- start
-pm2 startup
-pm2 save
-pm2 restart all
--- server/scripts/deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /home/ubuntu/github_action
-npm install --legacy-peer-deps
-npm run start
